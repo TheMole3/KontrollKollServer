968db541f9f2ff69c8151f53ea614e7e5fedd671
TheMole3 <hugomelo04@gmail.com>
2021-02-02 15:44:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -31,38 +31,16 @@ const handleError = (err, res) => {
 };
 
 const upload = multer({
-  dest: "/tempPics"
+  dest: "./tempPics"
   // you might also want to set some limits: https://github.com/expressjs/multer#limits
 });
 
-
 app.post(
-  "/finishTask",
-  upload.single("file" /* name attribute of <file> element in your form */),
-  (req, res) => {
-    const tempPath = req.file.path;
-    const targetPath = path.join(__dirname, "./uploads/image.png");
-
-    if (path.extname(req.file.originalname).toLowerCase() === ".png") {
-      fs.rename(tempPath, targetPath, err => {
-        if (err) return handleError(err, res);
-
-        res
-          .status(200)
-          .contentType("text/plain")
-          .end("File uploaded!");
-      });
-    } else {
-      fs.unlink(tempPath, err => {
-        if (err) return handleError(err, res);
-
-        res
-          .status(403)
-          .contentType("text/plain")
-          .end("Only .png files are allowed!");
-      });
+    "/finishTask",
+    upload.single("file" /* name attribute of <file> element in your form */),
+    (req, res) => {
+        console.log(req.file)
     }
-  }
 )
 
 app.use(function(req, res, next){ // 404 response for non defined urls
