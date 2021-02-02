2a8a0b642f72472936fb171b69b2ec43d0890fc7
TheMole3 <hugomelo04@gmail.com>
2021-02-02 12:33:49 +0100

    Init

---
+++ app.js
@@ -0,0 +1,34 @@
+/* Get config file */
+configFile = require("./config.json") // Load configFile
+
+/* Start express, socket, mongo and helmet */
+fs = require('fs');
+var app = require('express')();
+
+var server = app.listen(configFile.port || 3006);
+
+var mongojs = require('mongojs');
+var db = mongojs(configFile.dbConnect, ['children']); // Import database TheMole and collections bombman and referrals
+
+
+app.get('/getTasks', (req, res) => {
+    var id = req.params.id;
+    res.send(db.children.find({id:id}))
+})
+
+
+app.use(function(req, res, next){ // 404 response for non defined urls
+    res.status(404);
+    // respond with html page
+    if (req.accepts('html')) {
+      res.send(404)
+      return;
+    }
+    // respond with json
+    if (req.accepts('json')) {
+      res.send({ error: 'Not found' });
+      return;
+    }
+    // default to plain-text. send()
+    res.type('txt').send('Not found');
+  });
